8bf5e35b5715243d5b378c2ff6452a2f65cfe2c2
muniasamy1909@gmail.com <muniasamy1909@gmail.com>
2025-02-07 15:17:33 +0530

    Feat : Add Bloc StateManagement

---
+++ lib/presentation/bloc/car_bloc.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rent_cars_app/domain/usecases/get_cars.dart';
+import 'package:rent_cars_app/presentation/bloc/car_event.dart';
+import 'package:rent_cars_app/presentation/bloc/car_state.dart';
+
+class CarBloc extends Bloc<CarEvent, CarState> {
+  final GetCars getCars;
+  CarBloc({required this.getCars}) : super(CarsLoading()) {
+    on<LoadCars>(
+      (event, emit) async {
+        emit(CarsLoading());
+
+        try {
+          final cars = await getCars.call();
+
+          emit(CarsLoaded(cars));
+        } catch (e) {
+          emit(CarsError(e.toString()));
+        }
+      },
+    );
+  }
+}
